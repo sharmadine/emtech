9e80c8734572c64150e4717b6a3cfbf03bc67df0
Dinesh Sharma <sharmadine@gmail.com>
2016-03-16 02:31:19 +0530

    added spring boot eclipse project skeleton & config files

---
+++ boot_ms/src/main/java/com/dinesh/boot/BootMsApplication.java
@@ -0,0 +1,12 @@
+package com.dinesh.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMsApplication.class, args);
+	}
+}
---
+++ boot_ms/src/test/java/com/dinesh/boot/BootMsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dinesh.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BootMsApplication.class)
+public class BootMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
